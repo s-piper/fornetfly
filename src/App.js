792aadae385c289b493d84b2dcde6acb09dc6a5d
Stephen Piper <stephen.r.piper@gmail.com>
2021-07-19 01:27:03 -0500

    added project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+import Body from './Body';
+
+function App() {
+  return (
+    <div className="App">
+
+      <Header />
+
+      <Body />
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Body.js
@@ -0,0 +1,26 @@
+import Projects from './Projects';
+
+function Body() {
+
+    return (
+        <div>
+            <p>
+                Software Engineer with a background collaboratively communicating in the 3D Print Industry.
+            </p>
+            <p>
+                Life long learner currently working with React, Redux, Node, and SQL.
+            </p>
+
+
+            <div>
+                <h2>Projects</h2>
+
+            <Projects />
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Body;
